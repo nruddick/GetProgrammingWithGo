4b7e0ef452fb3d8694f6f55d11c96e9049d5a8f9
Nathan Ruddick <nathanruddick@gmail.com>
2019-11-04 23:01:44 -0800

    ch3

---
+++ 03-loops-and-branches/03-01-contains.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    fmt.Println("You find yourself in a dimly lit cavern.")
+
+    var command = "walk outside"
+    var exit = strings.Contains(command, "outside")
+
+    fmt.Println("You leave the cave:", exit)
+
+	// var wearShades = true
+	// var read = strings.Contains(command, "read")
+ }
+
