cc0927d189240326eee765fe55d738b33535a7b0
Nathan Ruddick <nathanruddick@gmail.com>
2019-11-05 13:02:48 -0800

    update 03-experiment.go

--- 03-loops-and-branches/03-experiment-guess.go
+++ 03-loops-and-branches/03-experiment-guess.go
@@ -25,22 +25,26 @@ func main() {
 		} else if compGuess > number {
 			fmt.Printf("%v is too high\n", compGuess)
 			guessMax = compGuess - 1
-			compGuess = rand.Intn(guessMax - guessMin) + guessMin	
+			compGuess = ((guessMax  + guessMin)/2)	
 		} else if compGuess < number {
 			fmt.Printf("%v is too low\n", compGuess)
 			guessMin = compGuess + 1
-			compGuess = rand.Intn(guessMax - guessMin) + guessMin
-		} else if compGuess > guessMax {
-			fmt.Printf("%v is too high and is not a number between %v and %v\n", compGuess, guessMin, guessMax)
-		} else if compGuess < guessMin {
-			fmt.Printf("%v is too low and is not a number between %v and %v\n", compGuess, guessMin, guessMax)
-		} else {
-			fmt.Printf("%v is not a number", compGuess)
+			compGuess = ((guessMax + guessMin)/2)
+		// } else if compGuess > guessMax {
+		// 	fmt.Printf("%v is too high and is not a number between %v and %v\n", compGuess, guessMin, guessMax)
+		// } else if compGuess < guessMin {
+		// 	fmt.Printf("%v is too low and is not a number between %v and %v\n", compGuess, guessMin, guessMax)
+		// } else {
+		// 	fmt.Printf("%v is not a number", compGuess)
 		}
 		time.Sleep(time.Second)
 	}
 	fmt.Printf("%v was the number!\n", number)
 }
+
+
+// log the time it takes to solve with binary search vs random generator with range limitations
+// make testing chart for thousands of tests to compare
 
 
 
