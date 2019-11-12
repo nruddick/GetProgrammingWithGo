b01b41e8d8ad50f140f3dc979553219b129d74b3
Nathan Ruddick <nathanruddick@gmail.com>
2019-11-12 02:15:43 -0800

    updating

---
+++ 05-capstone/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+    // distance in km
+    const distanceToMars = 62100000
+    // speed in km/s
+    const speedMin = 16
+    const speedMax = 30
+    // a 16 speed price min
+    const priceMin = 36
+    // a 30 speed price max
+    const priceMax = 50
+
+    i := 0
+
+    fmt.Printf("%-17v %-4v %-15v %v \n", "Spaceline", "Days", "Trip type", "Price")
+    fmt.Println("==============================================")
+
+    for i < 10 {
+
+        spaceLineId := rand.Intn(3)
+        spaceLine := ""
+        tripType := "One-way"
+
+        speed := speedMin + rand.Intn(speedMax-speedMin+1)
+
+        // using proportion to calculate price
+        price := priceMin + (speed-speedMin)*(priceMax-priceMin)/(speedMax-speedMin)
+
+        // seconds
+        tripDuration := distanceToMars / speed
+        // days
+        tripDuration = tripDuration / (60 * 60 * 24)
+
+        if rand.Intn(2) == 1 {
+            tripType = "Round-trip"
+            price *= 2
+        }
+
+        switch spaceLineId {
+        case 0:
+            spaceLine = "Space Adventures"
+        case 1:
+            spaceLine = "SpaceX"
+        case 2:
+            spaceLine = "Virgin Galactic"
+        }
+
+        fmt.Printf("%-17v %-4v %-15v $ %4v \n", spaceLine, tripDuration, tripType, price)
+
+        i++
+    }
+}
+
+
+// Spaceline        Days Trip type  Price
+// ======================================
+// Virgin Galactic    23 Round-trip $  96
+// Virgin Galactic    39 One-way    $  37
+// SpaceX             31 One-way    $  41
+// Space Adventures   22 Round-trip $ 100
+// Space Adventures   22 One-way    $  50
+// Virgin Galactic    30 Round-trip $  84
+// Virgin Galactic    24 Round-trip $  94
+// Space Adventures   27 One-way    $  44
+// Space Adventures   28 Round-trip $  86
+// SpaceX             41 Round-trip $  72
